7f71d42148c63562f262fcce596c5f8242353091
Deepak <deepakmr815@gmail.com>
2025-03-26 20:16:00 +0530

    some changes

--- ReactJs/day1 Repeat/src/App.jsx
+++ ReactJs/day1 Repeat/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ProfileCard from './components/ProfileCard'
 import './App.css'
-import Sarab from './Sarab'
-import Vaibhav from './Vaibhav'
 
 function App() {
 
@@ -14,9 +12,7 @@ function App() {
       <Card name="PIET" color="red"/>
       <Card name="Rohan" color="green"/>
       <ProfileCard/>
-      <Sarab name="sarab"/>
       <button onClick={fun}>click</button>
-      <Vaibhav name="Vikas"/>
    </> 
   )
 }
